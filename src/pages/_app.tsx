5c857b0170c5d77a11ffa82d4eac0cd1e992a391
Wonseok Yang <wsyang@digicaps.com>
2023-07-21 18:13:41 +0900

    nextjs setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,46 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    cursor: pointer;
+    text-decoration: none;
+    transition: .25s;
+    color:#000;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+        />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
